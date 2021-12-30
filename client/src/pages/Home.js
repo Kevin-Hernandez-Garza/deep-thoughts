049bd7268e5ac854ac5efe4783414d1aeca28d3e
Kevin Hernandez <kev.hernandezgarza@gmail.com>
2021-12-29 20:36:32 -0600

    thoughtForm created

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -6,6 +6,8 @@ import FriendList from '../components/FriendList';
 // allow us to make requests to the GraphQL server we connected to
 import { useQuery } from '@apollo/client';
 import { QUERY_THOUGHTS, QUERY_ME_BASIC } from '../utils/queries';
+// thoughtForm component import
+import ThoughtForm from '../components/ThoughtForm';
 import ThoughtList from '../components/ThoughtList';
 
 const Home = () => {
@@ -24,6 +26,11 @@ const Home = () => {
   return (
     <main>
       <div className='flex-row justify-space-between'>
+        {loggedIn && (
+          <div className='col-12 mb-3'>
+            <ThoughtForm />
+          </div>
+        )}
         <div className={`col-12 mb-3 ${loggedIn && 'col-lg-8'}`}>
           {loading ? (
             <div>Loading...</div>
